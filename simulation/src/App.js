abc2ef3ecea5542e0f2395b3b03df9158270265d
José Adolfo <jmicalco@gmail.com>
2020-11-15 20:46:13 -0600

    wrapper

--- simulation/src/App.js
+++ simulation/src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="one"></div>
+        <div className="wrapper">
+        <div className=""><h1>valor</h1></div>
+        <div className=""><h1>hola</h1></div>
+        <div className="">
+          <h1>side</h1>
+          <h1>side</h1>
+          <h1>side</h1>
+        </div>
+        <div className="">
+          <h1>cont</h1>
+          <h1>cont</h1>
+        </div>
+        <div className="">
+          <h1>footer</h1>
+        </div>
+      </div>
     </div>
   );
 }
